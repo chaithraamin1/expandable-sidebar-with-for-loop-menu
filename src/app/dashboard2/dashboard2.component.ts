4f70a0cea9da45f479765ed3d204f63a8801116f
chai <chaithra.amin98@gmail.com>
2022-09-27 13:19:27 +0530

    sidebar menu with for loop and keep active link using for loop

--- src/app/dashboard2/dashboard2.component.ts
+++ src/app/dashboard2/dashboard2.component.ts
@@ -6,6 +6,9 @@ import * as $ from "jquery";
   styleUrls: ['./dashboard2.component.scss']
 })
 export class Dashboard2Component implements OnInit {
+  menus: any = [{ displayName: "Home", _id: "home", icon: "/assets/home.png", blue_icon: "/assets/home_blue.png" }, { displayName: "Scheduler", _id: "Scheduler", icon: "/assets/plans.png", blue_icon: "/assets/plans_blue.png" }, { displayName: "Notes", _id: "Notes", icon: "/assets/edit.png", blue_icon: "/assets/edit_blue.png" }, { displayName: "Functions", _id: "Functions", icon: "/assets/plans.png", blue_icon: "/assets/plans_blue.png" }, { displayName: "Account", _id: "Account", icon: "/assets/account.png", blue_icon: "/assets/account_blue.png" }]
+  selectedMenu: any;
+  
 
   constructor() { }
 
@@ -22,6 +25,13 @@ export class Dashboard2Component implements OnInit {
       sidebar.classList.toggle("closed");
     });
 
+    for(var i=0;i<this.menus.length;i++){
+      this.selectedMenu=this.menus[0];
+    }
+   }
+  onSelect(menu:any): void {
+   this.selectedMenu = menu;
+   console.log("selcted menu",this.selectedMenu )
   }
 
 }
